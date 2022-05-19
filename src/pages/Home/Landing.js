a6db57f2c6f9fede4ecd529fc4285722778a6df3
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-05-19 17:28:13 +0600

    input filed added

--- src/pages/Home/Landing.js
+++ src/pages/Home/Landing.js
@@ -1,25 +1,74 @@
 import React from "react";
+import BucketGirl from "../../assets/iamages/bucketgirl.png";
 
 const Landing = () => {
   return (
-    <div class="hero min-h-screen bg-base-200 mt-16">
-      <div class="hero-content flex-col lg:flex-row">
-        <div>
-          <h1 class="text-5xl font-bold">Box Office News!</h1>
-          <p class="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button class="btn btn-primary">Get Started</button>
+    <>
+      <div class="hero h-screen lg:h-[60vh] bg-accent relative z-0 mt-16">
+        <div class="hero-content flex-col lg:flex-row">
+          <div>
+            <p className="text-xl text-white">Best Quality</p>
+            <h1 class="text-5xl font-bold text-white">
+              Professionaol Cleaning Service
+            </h1>
+            <p class="py-6 max-w-xl text-white">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi
+              repudiandae sit aliquam sed voluptate fugiat.
+            </p>
+            <button class="btn btn-primary">Get Started</button>
+          </div>
+          <div className="h-[60vh] shrink-0">
+            <img src={BucketGirl} class="h-full" alt="" />
+          </div>
         </div>
-        <img
-          src="https://api.lorem.space/image/movie?w=260&h=400 "
-          alt=""
-          class="max-w-sm rounded-lg shadow-2xl"
-        />
       </div>
-    </div>
+
+      <div className="rounded-2xl mx-auto mt-[-50px] relative z-10 shadow-lg p-20 w-5/6">
+        <h1>Get Free Estimate</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full "
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full "
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full "
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full "
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full "
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full "
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full "
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full "
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
